965e1d7bf5502ac8e75b4f9155a8e0a2bfdb9b86
Sergei Malyshev <msvclimber@yandex.ru>
2022-06-15 15:56:54 +0400

    Create users.js

---
+++ src/data/users.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    login: "tatyana.yarovova@yandex.ru",
+    name: "Яровова Татьяна",
+    password: "123Tanya",
+    shopId: 1,
+  },
+  {
+    id: 2,
+    login: "eanastasiya@yandex.ru",
+    name: "Егорова Анастасия",
+    password: "123Nastya",
+    shopId: null,
+  },
+  {
+    id: 3,
+    login: "qwerty",
+    name: "Вася пупкин",
+    password: "qwerty",
+    shopId: 1,
+  },
+];
